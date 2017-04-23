376c6c97e7a08bd559b754186d3569f9384dab19
aillieo <aillieo@163.com>
2017-04-23 20:28:55 +0800

    minor update

--- CLIENT/unity_proj/GAME2017/Assets/Scripts/Modules/Dash/PanelUserData.cs
+++ CLIENT/unity_proj/GAME2017/Assets/Scripts/Modules/Dash/PanelUserData.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PanelUserData : PanelBase {
+public class PanelUserData : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ CLIENT/unity_proj/GAME2017/Assets/Scripts/Modules/Global/UIManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GAME2017;
+
+namespace GAME2017
+{
+	public class UIManager :SingletonMonoBehaviour<UIManager>{
+
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+			
+
+		}
+
+
+		public void showWaiting(float waitTime)
+		{
+			
+		}
+
+		public void showAlert(string alertText)
+		{
+			
+		}
+
+		public void showConfirm(string confirmText, System.Delegate confirmCallback)
+		{
+			
+		}
+
+		public void showTip(string tipText, float duration)
+		{
+			
+		}
+
+	}
+
+}
