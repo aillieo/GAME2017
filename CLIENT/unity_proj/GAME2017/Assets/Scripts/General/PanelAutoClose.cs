1d075188c5f5d0a96fdd219ed77d4413abf6b85c
aillieo <aillieo@163.com>
2017-04-27 11:55:25 +0800

    ui panel auto close when clicked outside

--- CLIENT/unity_proj/GAME2017/Assets/Scripts/General/PanelAutoClose.cs
+++ CLIENT/unity_proj/GAME2017/Assets/Scripts/General/PanelAutoClose.cs
@@ -1,36 +1,97 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class PanelAutoClose : MonoBehaviour {
 
-
-	protected GameObject _panel;
-
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 
-		_panel = this.gameObject;
+		AddAutoCloseToPanel ();
+
+		AddEventPointerClickToChildren ();
+
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 
-		if (Input.GetMouseButtonDown (0)) 
-		{
-			//Vector2 pos =  Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject () ){
-				GAME2017.UIManager.Instance.ClosePanel ();
-				//Debug.Log ("out");
-				// if pos locate outside the panel 
-				// set panel inactive
-			} else {
-				//Debug.Log ("in");
-			}
+	}
 
+	public void OnScriptControll(BaseEventData arg0)
+	{
+		//Debug.Log("PanelAutoClose");
+		GAME2017.UIManager.Instance.ClosePanel();
+	}
+
+	void AddAutoCloseToPanel()
+	{
+		// add auto-close to panel 
+		var trigger = transform.gameObject.GetComponent<EventTrigger>();
+		if (trigger == null)
+			trigger = transform.gameObject.AddComponent<EventTrigger>();
+
+		// init triggers
+		if (trigger.triggers == null) 
+			trigger.triggers = new List<EventTrigger.Entry> ();
+
+		EventTrigger.Entry entryPointerClick = null;
+		foreach(var e in trigger.triggers)
+		{
+			if (e.eventID == EventTriggerType.PointerClick) {
+				entryPointerClick = e;
+				break;
+			}
+		}
+
+		UnityAction<BaseEventData> callback = new UnityAction<BaseEventData> (OnScriptControll);
+
+		if (null == entryPointerClick) {
+
+			EventTrigger.Entry entry = new EventTrigger.Entry ();
+			entry.eventID = EventTriggerType.PointerClick;
+			trigger.triggers.Add (entry);
+
+			entry.callback = new EventTrigger.TriggerEvent ();
+			entry.callback.AddListener (callback);
+
+		} 
+		else {
+			if(entryPointerClick.callback == null)
+			{
+				entryPointerClick.callback = new EventTrigger.TriggerEvent ();
+			}
+			entryPointerClick.callback.AddListener (callback);
 		}
 
 	}
-		
+
+	void AddEventPointerClickToChildren()
+	{
+		// add pointer-click to all child nodes
+		foreach (Transform child in gameObject.transform)  
+		{  
+			var trigger = child.gameObject.GetComponent<EventTrigger>();
+			if (trigger == null)
+				trigger = child.gameObject.AddComponent<EventTrigger>();
+
+			// init triggers
+			if (trigger.triggers == null) 
+				trigger.triggers = new List<EventTrigger.Entry> ();
+
+			EventTrigger.Entry entryPointerClick = null;
+			foreach(var e in trigger.triggers)
+			{
+				if (e.eventID == EventTriggerType.PointerClick) {
+					entryPointerClick = e;
+					break;
+				}
+			}
+
+		} 
+	}
 }
