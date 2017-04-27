f6b5afabeea52366410fc45ab5d874a35071f622
aillieo <aillieo@163.com>
2017-04-27 19:55:58 +0800

    minor update for dash-UI

---
+++ CLIENT/unity_proj/GAME2017/Assets/Scripts/Modules/Dash/PanelChest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GAME2017
+{
+	public class PanelChest : MonoBehaviour {
+
+		// Use this for initialization
+		void Start () {
+
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+	}
+}
